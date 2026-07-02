9f91a02f2cc9950b64712f367565f25d552cbd16
PainterFlow11 <163044531+PainterFlow@users.noreply.github.com>
2026-07-02 20:50:42 +0200

    finished button improvments

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,13 @@
 const variants = {
-  primary:
-    "border-button-primary-outline bg-button-primary text-button-text hover:border-button-primary-outline-hover hover:bg-button-primary-hover",
-  secondary:
-    "border-button-secondary-outline bg-button-secondary text-button-text hover:border-button-secondary-outline-hover hover:bg-button-secondary-hover",
-  blue:
-    "border-button-blue-outline bg-button-blue text-button-text hover:border-button-blue-outline-hover hover:bg-button-blue-hover",
-  purple:
-    "border-button-purple-outline bg-button-purple text-button-text hover:border-button-purple-outline-hover hover:bg-button-purple-hover",
+  primary: "border-button-primary-outline bg-button-primary text-button-text hover:border-button-primary-outline-hover hover:bg-button-primary-hover",
+  secondary: "border-button-secondary-outline bg-button-secondary text-button-text hover:border-button-secondary-outline-hover hover:bg-button-secondary-hover",
+  blue: "border-button-blue-outline bg-button-blue text-button-text hover:border-button-blue-outline-hover hover:bg-button-blue-hover",
+  purple: "border-button-purple-outline bg-button-purple text-button-text hover:border-button-purple-outline-hover hover:bg-button-purple-hover",
 };
 
 const sizes = {
-  sm: "rounded-md px-4 py-2 text-sm",
-  md: "rounded-lg px-6 py-3 text-base",
+  sm: "rounded-lg px-4 py-2 text-sm",
+  md: "rounded-xl px-6 py-3 text-base",
 };
 
 export default function Button({
