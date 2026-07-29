1245818f10570675f2a171527e73a51a5b1f6bcc
PainterFlow11 <163044531+PainterFlow@users.noreply.github.com>
2026-07-29 20:39:00 +0200

    added a new /news page which is empty for now

---
+++ src/app/news/page.js
@@ -0,0 +1,9 @@
+import DefaultTemplatePage from "@/template-pages/DefaultTemplatePage";
+
+export const metadata = {
+  title: "News | Vanilla²",
+};
+
+export default function NewsPage() {
+  return <DefaultTemplatePage />;
+}
