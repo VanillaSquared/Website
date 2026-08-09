b8f3420b4adb083e716ac65ce6b087acc127f027
PainterFlow11 <163044531+PainterFlow@users.noreply.github.com>
2026-08-09 15:09:15 +0200

    Try 1% iOS filter sidebar animation offset

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -169,8 +169,8 @@ export default function Modal({
 
     const animation = dialog.animate(
       open
-        ? [{ transform: "translateX(0.01%)" }, { transform: "translateX(0)" }]
-        : [{ transform: "translateX(0)" }, { transform: "translateX(0.01%)" }],
+        ? [{ transform: "translateX(1%)" }, { transform: "translateX(0)" }]
+        : [{ transform: "translateX(0)" }, { transform: "translateX(1%)" }],
       {
         duration: MODAL_ANIMATIONS["slide-right"].duration,
         easing: open ? "ease-out" : "ease-in",
